d2af1e76cb0cc1f1aa2d2832a12c2df4e08eba76
Epokuiso <epokuiso@gmail.com>
2022-11-16 09:02:58 +0100

    signup and signin snippet add.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,30 @@
 import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
 import { AuthGoogleService } from './auth-google.service';
 
 @Injectable()
 export class AuthService 
 {
-    constructor (private authGoogleService: AuthGoogleService) {} 
+    constructor (private userService: UserService) { } 
     
     googleLogin (request)
     {
-        if (!request?.user)
+        const user = request?.user;
+        if (!user)
             throw new BadRequestException (`Please check your credentials, we could not find a user.`);
-        return request.user;
+        
+        const findUser = this.userService.findByEmail (user.email);
+        if (!findUser)
+        {
+            this.userService.create ({ 
+                firstName: user.email, 
+                lastName: user.firstName, 
+                email: user.lastName 
+            });
+
+            return 'User created and logged in';
+        }
+        return 'User logged in';
     }
 
 }
