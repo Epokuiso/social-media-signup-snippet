cdbc97f6cdf9b3ba5a96b9b804cae859c99216c3
Emanuel Moura <79352091+Epokuiso@users.noreply.github.com>
2022-11-17 21:35:58 +0100

    imports --fix

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { UserService } from 'src/user/user.service';
+import { UserModule } from 'src/user/user.module';
 import { AuthGoogleService } from './auth-google.service';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
-  imports: [UserService],
+  imports: [UserModule],
   controllers: [AuthController],
   providers: [AuthService, AuthGoogleService]
 })
